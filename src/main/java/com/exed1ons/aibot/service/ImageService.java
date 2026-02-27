be1d9f17f92bde30658578edc37d9b9a74b637dd
Yaroslav <yomelchenko.work@gmail.com>
2026-02-27 02:36:55 +0100

    zimage

--- src/main/java/com/exed1ons/aibot/service/ImageService.java
+++ src/main/java/com/exed1ons/aibot/service/ImageService.java
@@ -31,7 +31,7 @@ public class ImageService {
             String fullPrompt = appearancePrompt + ", " + promptContext;
 
             URI uri = UriComponentsBuilder.fromHttpUrl("https://gen.pollinations.ai/image/{prompt}")
-                    .queryParam("model", "flux")
+                    .queryParam("model", "zimage")
                     .queryParam("width", 1024)
                     .queryParam("height", 1024)
                     .queryParam("nologo", "true")
